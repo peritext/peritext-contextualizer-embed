121d7e43740e200cf31aed3ea35a152674c5d2fe
Robin de Mourat <robin.demourat@gmail.com>
2017-12-01 08:41:46 +0100

    add precommit hook

--- dist/BlockStatic.js
+++ dist/BlockStatic.js
@@ -1,29 +1,46 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _react = require("react");
+var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = function (_ref) {
+var BlockStatic = function BlockStatic(_ref, _ref2) {
   var resource = _ref.resource;
+  var datasets = _ref2.datasets;
 
-  if (resource.data.thumbnail) {
+  var data = resource.data;
+  var thumbnail = datasets[data.thumbnailDataset];
+  if (thumbnail) {
     return _react2.default.createElement(
-      "figure",
-      { className: "resource-thumbnail peritext-contextualization peritext-contextualization-block peritext-contextualization-codex peritext-contextualizer-embed" },
-      _react2.default.createElement("img", {
-        src: resource.data.thumbnail
+      'figure',
+      { className: 'resource-thumbnail peritext-contextualization peritext-contextualization-block peritext-contextualization-codex peritext-contextualizer-embed' },
+      _react2.default.createElement('img', {
+        src: thumbnail.uri
       })
     );
-  } else return _react2.default.createElement("figure", {
-    className: "peritext-contextualization peritext-contextualization-block peritext-contextualization-codex peritext-contextualizer-embed",
+  } else return data.html ? _react2.default.createElement('figure', {
+    className: 'peritext-contextualization peritext-contextualization-block peritext-contextualization-codex peritext-contextualizer-embed',
     dangerouslySetInnerHTML: {
-      __html: resource.data.html
-    } });
+      __html: data.html
+    } }) : null;
 };
+
+BlockStatic.propTypes = {
+  resource: _propTypes2.default.object
+};
+
+BlockStatic.contextTypes = {
+  datasets: _propTypes2.default.object
+};
+
+exports.default = BlockStatic;
