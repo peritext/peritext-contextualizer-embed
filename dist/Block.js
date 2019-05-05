0c66b16834eb46eeffa881d6e39082e7ad77b2c2
Robin de Mourat <robin.demourat@gmail.com>
2019-05-05 14:14:26 +0200

    add fullscreen feature

--- dist/Block.js
+++ dist/Block.js
@@ -5,15 +5,62 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _reactFullScreen = _interopRequireDefault(require("react-full-screen"));
 
 var _meta = _interopRequireDefault(require("./meta"));
 
 var _peritextUtils = require("peritext-utils");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+class FullableEmbed extends _react.Component {
+  constructor(props) {
+    super(props);
+
+    _defineProperty(this, "render", () => {
+      const {
+        html
+      } = this.props;
+      return [_react.default.createElement(_reactFullScreen.default, {
+        key: 0,
+        enabled: this.state.isFullScreen,
+        onChange: isFullScreen => this.setState({
+          isFullScreen
+        })
+      }, _react.default.createElement("div", {
+        dangerouslySetInnerHTML: {
+          /* eslint react/no-danger : 0 */
+          __html: html
+        }
+      }), this.state.isFullScreen && _react.default.createElement("button", {
+        key: 1,
+        className: "fullscreen-btn",
+        onClick: () => this.setState({
+          isFullScreen: false
+        })
+      }, "\u2756")), _react.default.createElement("button", {
+        key: 1,
+        className: "fullscreen-btn",
+        onClick: () => this.setState({
+          isFullScreen: true
+        })
+      }, "\u2756")];
+    });
+
+    this.state = {
+      isFullScreen: false
+    };
+  }
+
+}
 
 const Block = ({
   resource,
@@ -37,11 +84,8 @@ const Block = ({
   const renderContent = () => {
     switch (field) {
       case 'html':
-        return _react.default.createElement("div", {
-          dangerouslySetInnerHTML: {
-            /* eslint react/no-danger : 0 */
-            __html: resource.data.html
-          }
+        return _react.default.createElement(FullableEmbed, {
+          html: resource.data.html
         });
 
       default:
--- src/Block.js
+++ src/Block.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Fullscreen from "react-full-screen";
 
 import meta from './meta';
 import { chooseAppropriateAsset } from 'peritext-utils';
+
+class FullableEmbed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFullScreen: false
+    }
+  }
+  render = () => {
+    const {
+      html
+    } = this.props;
+    return [
+      <Fullscreen
+        key={0}
+        enabled={this.state.isFullScreen}
+        onChange={isFullScreen => this.setState({isFullScreen})}
+      >
+      <div
+        dangerouslySetInnerHTML={ {/* eslint react/no-danger : 0 */
+          __html: html
+        } }
+      />
+      {this.state.isFullScreen && 
+        <button 
+          key={1} 
+          className="fullscreen-btn" 
+          onClick={() => this.setState({isFullScreen: false})}
+        >
+          ❖
+        </button>
+      }
+      </Fullscreen>,
+      <button 
+        key={1} 
+        className="fullscreen-btn" 
+        onClick={() => this.setState({isFullScreen: true})}
+      >
+        ❖
+      </button>
+    ]
+  }
+}
 
 const Block = ( {
   resource,
@@ -28,11 +72,7 @@ const Block = ( {
     switch ( field ) {
       case 'html':
         return (
-          <div
-            dangerouslySetInnerHTML={ {/* eslint react/no-danger : 0 */
-              __html: resource.data.html
-            } }
-          />
+          <FullableEmbed html={resource.data.html} />
         );
 
         default:
