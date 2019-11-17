8387deb7c250eae3635f811b8f229f3b37e05900
Robin de Mourat <robin.demourat@gmail.com>
2019-11-17 16:30:56 +0100

    add two-steps display

--- dist/Block.js
+++ dist/Block.js
@@ -19,48 +19,29 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-class FullableEmbed extends _react.Component {
-  constructor(props) {
-    super(props);
-
-    _defineProperty(this, "render", () => {
-      const {
-        html
-      } = this.props;
-      return [_react.default.createElement(_reactFullScreen.default, {
-        key: 0,
-        enabled: this.state.isFullScreen,
-        onChange: isFullScreen => this.setState({
-          isFullScreen
-        })
-      }, _react.default.createElement("div", {
-        dangerouslySetInnerHTML: {
-          /* eslint react/no-danger : 0 */
-          __html: html
-        }
-      }), this.state.isFullScreen && _react.default.createElement("button", {
-        key: 1,
-        className: "fullscreen-btn",
-        onClick: () => this.setState({
-          isFullScreen: false
-        })
-      }, "\u2756")), _react.default.createElement("button", {
-        key: 1,
-        className: "fullscreen-btn",
-        onClick: () => this.setState({
-          isFullScreen: true
-        })
-      }, "\u2756")];
-    });
-
-    this.state = {
-      isFullScreen: false
-    };
-  }
-
-}
+const FullableEmbed = ({
+  html
+}) => {
+  const [isFullScreen, setFullScreen] = (0, _react.useState)(false);
+  return [_react.default.createElement(_reactFullScreen.default, {
+    key: 0,
+    enabled: isFullScreen,
+    onChange: newVal => setFullScreen(newVal)
+  }, _react.default.createElement("div", {
+    dangerouslySetInnerHTML: {
+      /* eslint react/no-danger : 0 */
+      __html: html
+    }
+  }), isFullScreen && _react.default.createElement("button", {
+    key: 1,
+    className: "fullscreen-btn",
+    onClick: () => setFullScreen(false)
+  }, "\u2756")), _react.default.createElement("button", {
+    key: 1,
+    className: "fullscreen-btn",
+    onClick: () => setFullScreen(true)
+  }, "\u2756")];
+};
 
 const Block = ({
   resource,
@@ -70,40 +51,60 @@ const Block = ({
 }, {
   productionAssets
 }) => {
-  const appropriateAsset = (0, _peritextUtils.chooseAppropriateAsset)(resource, _meta.default.profile.block.assetPickingRules.element[renderingMode], productionAssets);
-  let field;
+  const title = contextualization.title || (0, _peritextUtils.getResourceTitle)(resource);
+  const [isLocked, setLocked] = (0, _react.useState)(true);
+  const coverAsset = (0, _peritextUtils.chooseAppropriateAsset)(resource, _meta.default.profile.block.assetPickingRules.element['paged'], productionAssets);
+  let coverField;
 
-  if (appropriateAsset) {
-    field = appropriateAsset.resourceDataField;
+  if (coverAsset) {
+    coverField = coverAsset.resourceDataField;
   } else {
     return null;
   }
 
   let assetUri;
+  let activeAssetField = coverField;
+  let Content = null;
 
-  const renderContent = () => {
-    switch (field) {
-      case 'html':
-        return _react.default.createElement(FullableEmbed, {
-          html: resource.data.html
-        });
+  if (renderingMode === 'paged') {
+    if (coverAsset && coverAsset.asset) {
+      assetUri = coverAsset.asset.data;
+      activeAssetField = coverField;
 
-      default:
-        if (appropriateAsset) {
-          assetUri = appropriateAsset.asset.data;
-          return _react.default.createElement("img", {
-            src: assetUri
-          });
-        }
-
-        return null;
+      Content = () => _react.default.createElement("img", {
+        src: assetUri
+      });
     }
-  };
+  } else if (coverAsset && coverAsset.asset) {
+    if (isLocked) {
+      assetUri = coverAsset.asset.data;
+      activeAssetField = coverField;
+
+      Content = () => _react.default.createElement("div", {
+        onClick: () => setLocked(false),
+        className: "cover"
+      }, _react.default.createElement("img", {
+        src: assetUri
+      }), _react.default.createElement("h3", null, title));
+    } else {
+      activeAssetField = 'html';
+
+      Content = () => _react.default.createElement(FullableEmbed, {
+        html: resource.data.html
+      });
+    }
+  } else {
+    activeAssetField = 'html';
+
+    Content = () => _react.default.createElement(FullableEmbed, {
+      html: resource.data.html
+    });
+  }
 
   return _react.default.createElement("div", {
     id: contextualization.id,
-    className: `peritext-contextualization block embed rendering-mode-${renderingMode} asset-field-${field}`
-  }, renderContent()); // }
+    className: `peritext-contextualization block embed rendering-mode-${renderingMode} asset-field-${activeAssetField}`
+  }, _react.default.createElement(Content, null));
 };
 
 Block.contextTypes = {
--- src/Block.js
+++ src/Block.js
@@ -1,51 +1,43 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import PropTypes from 'prop-types';
 import Fullscreen from "react-full-screen";
 
 import meta from './meta';
-import { chooseAppropriateAsset } from 'peritext-utils';
+import { chooseAppropriateAsset, getResourceTitle } from 'peritext-utils';
 
-class FullableEmbed extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isFullScreen: false
-    }
-  }
-  render = () => {
-    const {
-      html
-    } = this.props;
-    return [
-      <Fullscreen
-        key={0}
-        enabled={this.state.isFullScreen}
-        onChange={isFullScreen => this.setState({isFullScreen})}
-      >
-      <div
-        dangerouslySetInnerHTML={ {/* eslint react/no-danger : 0 */
-          __html: html
-        } }
-      />
-      {this.state.isFullScreen && 
-        <button 
-          key={1} 
-          className="fullscreen-btn" 
-          onClick={() => this.setState({isFullScreen: false})}
-        >
-          ❖
-        </button>
-      }
-      </Fullscreen>,
+const FullableEmbed = ({
+  html
+}) => {
+  const [isFullScreen, setFullScreen] = useState(false);
+  return [
+    <Fullscreen
+      key={0}
+      enabled={isFullScreen}
+      onChange={newVal => setFullScreen(newVal)}
+    >
+    <div
+      dangerouslySetInnerHTML={ {/* eslint react/no-danger : 0 */
+        __html: html
+      } }
+    />
+    {isFullScreen && 
       <button 
         key={1} 
         className="fullscreen-btn" 
-        onClick={() => this.setState({isFullScreen: true})}
+        onClick={() => setFullScreen(false)}
       >
         ❖
       </button>
-    ]
-  }
+    }
+    </Fullscreen>,
+    <button 
+      key={1} 
+      className="fullscreen-btn" 
+      onClick={() => setFullScreen(true)}
+    >
+      ❖
+    </button>
+  ]
 }
 
 const Block = ( {
@@ -57,43 +49,60 @@ const Block = ( {
   productionAssets,
 } ) => {
 
-  const appropriateAsset = chooseAppropriateAsset( resource, meta.profile.block.assetPickingRules.element[renderingMode], productionAssets );
-  let field;
-  if ( appropriateAsset ) {
-    field = appropriateAsset.resourceDataField;
+  const title = contextualization.title || getResourceTitle(resource)
+
+  const [isLocked, setLocked] = useState(true)
+
+  const coverAsset = chooseAppropriateAsset( resource, meta.profile.block.assetPickingRules.element['paged'], productionAssets );
+  let coverField;
+  if ( coverAsset ) {
+    coverField = coverAsset.resourceDataField;
   }
- else {
+  else {
     return null;
   }
 
   let assetUri;
-
-  const renderContent = () => {
-    switch ( field ) {
-      case 'html':
-        return (
-          <FullableEmbed html={resource.data.html} />
-        );
-
-        default:
-          if ( appropriateAsset ) {
-            assetUri = appropriateAsset.asset.data;
-            return (
-              <img src={ assetUri } />
-            );
-          }
-        return null;
-      }
-    };
-    return (
-      <div
-        id={ contextualization.id }
-        className={ `peritext-contextualization block embed rendering-mode-${renderingMode} asset-field-${field}` }
-      >
-        {renderContent()}
-      </div>
+  let activeAssetField = coverField;
+  let Content = null;
+  if (renderingMode === 'paged') {
+    if ( coverAsset && coverAsset.asset ) {
+      assetUri = coverAsset.asset.data;
+      activeAssetField = coverField;
+      Content = () => (
+        <img src={ assetUri } />
+      );
+    }
+  } else if (coverAsset && coverAsset.asset) {
+    if (isLocked) {
+      assetUri = coverAsset.asset.data;
+      activeAssetField = coverField;
+      Content = () => (
+        <div onClick={() => setLocked(false)} className="cover">
+          <img src={assetUri} />
+          <h3>{title}</h3>
+        </div>
+      );
+    } else {        
+      activeAssetField = 'html';
+      Content = () => (
+        <FullableEmbed html={resource.data.html} />
+      )
+    }
+  } else {
+    activeAssetField = 'html';
+    Content = () => (
+      <FullableEmbed html={resource.data.html} />
     );
-  // }
+  }
+  return (
+    <div
+      id={ contextualization.id }
+      className={ `peritext-contextualization block embed rendering-mode-${renderingMode} asset-field-${activeAssetField}` }
+    >
+      <Content />
+    </div>
+  );
 };
 
 Block.contextTypes = {
