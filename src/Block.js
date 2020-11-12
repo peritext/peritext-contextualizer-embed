4b97648ea83bf3b3657d647647b7b3735e3d7a90
Robin de Mourat <robin.demourat@gmail.com>
2020-11-12 12:12:02 +0100

    0.1.5

--- src/Block.js
+++ src/Block.js
@@ -1,76 +1,9 @@
-import React, {useState, Component, useCallback} from 'react';
+import React, {useState, useCallback} from 'react';
 import PropTypes from 'prop-types';
 import {FullScreen, useFullScreenHandle} from "react-full-screen";
 
 import meta from './meta';
 import { chooseAppropriateAsset, getResourceTitle } from 'peritext-utils';
-
-/*
-class FullableEmbed extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isFullScreen: false
-    }
-  }
-  shouldComponentUpdate = (nextProps, nextState) => {
-    return (this.props.html !== nextProps.html) || (this.state.isFullScreen !== nextState.isFullScreen);
-  }
-  setFullScreen = (to) => {
-    this.setState({isFullScreen: to})
-  }
-
-  render () {
-    const {
-      props: {
-        html
-      },
-      state: {
-        isFullScreen,
-      },
-      setFullScreen,
-    } = this;
-    const screen = useFullScreenHandle();
-    const reportChange = useCallback((state) => {
-      setFullScreen(state.active)
-    }, [screen]);
-    return [
-      <Fullscreen
-        key={0}
-        handle={screen}
-        onChange={reportChange}
-      >
-      <div
-        dangerouslySetInnerHTML={ {
-          __html: html
-        } }
-      />
-      {isFullScreen && 
-        <button 
-          key={1} 
-          className="fullscreen-btn" 
-          onClick={() => {
-            setFullScreen(false)
-            screen.exit();
-          }}
-        >
-          ❖
-        </button>
-      }
-      </Fullscreen>,
-      <button 
-        key={1} 
-        className="fullscreen-btn" 
-        onClick={() => {
-          setFullScreen(true)
-          screen.enter();
-        }}
-      >
-        ❖
-      </button>
-    ];
-  }
-}*/
 
 const FullableEmbed = ({
   html
